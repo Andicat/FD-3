3fb791a08f64814d69ec8b1c28949f98ba7f4986
Andreeva <andikatya@gmail.com>
2020-08-31 18:35:41 +0300

    images, correct props products

--- ishop/Ishop.js
+++ ishop/Ishop.js
@@ -3,7 +3,7 @@
   displayName: 'ProductsTable',
 
   getDefaultProps: function() {
-    return { title: "Какой-то интернет-магазин" }
+    return { title: "Какой-то интернет-магазин", products: [] }
   },
 
   render: function() {
@@ -14,16 +14,15 @@
       React.DOM.div({className:'ProductsTitle'},
         React.DOM.span({className:'TitleName'},'Product'),  
         React.DOM.span({className:'TitlePrice'},'Price'),
-        React.DOM.span({className:'TitlePhoto'},'Photo'),
         React.DOM.span({className:'TitleCount'},'Count'),
       );
     
     this.props.products.forEach(element => {
       var productCode=        
         React.DOM.div({key:element.code,className:'Product'},
+          React.DOM.img({className:'Photo', src: element.photo, alt:element.photo, width:'60px', height:'60px'}),  
           React.DOM.span({className:'Title'},element.title),  
           React.DOM.span({className:'Price'},element.price),
-          React.DOM.a({className:'Photo', href: element.photo},element.photo),
           React.DOM.span({className:'Count'},element.count),
         );
       productsCode.push(productCode);
