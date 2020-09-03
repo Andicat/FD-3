566a65dd04c3196afb8e9cadeffd07fa0d4c3a6b
Katerina <andicat@mail.ru>
2020-09-03 11:37:54 +0300

    filter task finish

--- filter/Filter.js
+++ filter/Filter.js
@@ -7,28 +7,47 @@
   },
 
   getInitialState: function() {
-    return { optionsToShow:this.props.options }
+    return { optionsToShow:this.props.options.map(function (val) { return val; }),
+             isSorting:false,
+             textFilter:'', }
   },
 
-  sortOptions: function(EO) {
-    var isSorted = EO.target.checked;
-    //console.log(this.props.options);
-    var optionsUpdated = this.state.optionsToShow;
-    optionsUpdated.sort(function(a,b) {
-      return a.text>b.text? 1:a.text<b.text?-1:0;
-    });
-    console.log(this.props.options);
-    console.log(optionsUpdated);
-    //this.setState({optionsToShow:optionsUpdated});
+  filterOptions: function(EO) {
+    var textFilter;
+    var isSorting;
+    switch (EO.target.type) {
+      case 'text':
+        textFilter = EO.target.value;
+        isSorting = this.state.isSorting;
+        this.setState( {textFilter:textFilter} );
+        break;
+      case 'checkbox':
+        isSorting = EO.target.checked;
+        textFilter = this.state.textFilter;
+        this.setState( {isSorting:isSorting} );
+        break;
+      default:
+    }
+
+    var filteredArr = isSorting ? this.props.options.map(function (val) { return val; }).sort(function(a,b) { return a.text>b.text? 1:a.text<b.text?-1:0; }) : this.props.options.map(function (val) { return val; });
+    filteredArr = filteredArr.filter(function(val) { return val.text.toLowerCase().indexOf(textFilter.toLowerCase()) > -1; });
+    this.setState( {optionsToShow:filteredArr} );
+  },
+
+  resetOptions: function(EO) {
+    this.setState( {textFilter:''} );
+    this.setState( {isSorting:false} );
+    var filteredArr = this.props.options.map(function (val) { return val; });
+    this.setState( {optionsToShow:filteredArr} );
   },
 
   render: function() {
 
     var filterPanel=   
       React.DOM.div({className:'Filter__panel'},
-        React.DOM.input({className:'Filter__sort',type:'checkbox',onClick:this.sortOptions}),  
-        React.DOM.input({className:'Filter__search',type:'text'}),
-        React.DOM.button({className:'Filter__button'},'сброс'),
+        React.DOM.input({className:'Filter__sort',type:'checkbox',checked:this.state.isSorting,onClick:this.filterOptions}),  
+        React.DOM.input({className:'Filter__search',type:'text',value:this.state.textFilter,onChange:this.filterOptions}),
+        React.DOM.button({className:'Filter__button',onClick:this.resetOptions},'сброс'),
       );
 
       var optionsCode=[];
@@ -41,7 +60,7 @@
   
     var filterResult=
       React.DOM.select({className:'Filter__select',size:'5'},optionsCode);
-    
+
     return React.DOM.div( {className:'Filter'},filterPanel,filterResult);
   },
 
